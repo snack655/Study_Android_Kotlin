ca6817c41d50d2295d40810f28becbf2c0bea0d4
snack655 <cmjcmj1230@gmail.com>
2021-08-06 03:24:22 +0900

    2021-08-06 First Kotlin Test
    First Test -> Kotlin! Android!

---
+++ FirstKtProject/app/src/main/java/org/techtown/firstktproject/MainActivity.kt
@@ -0,0 +1,17 @@
+package org.techtown.firstktproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.techtown.firstktproject.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnSay.setOnClickListener {
+            binding.textSay.text = "Hello Kotlin!!!"
+        }
+    }
+}
