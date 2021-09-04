bc1de973f9d610e0c64dd198d688f86ae547b028
snack655 <cmjcmj1230@gmail.com>
2021-09-05 00:18:31 +0900

    Study : SharedPreferences
    2021-09-05
    f..u..

---
+++ SharedPreferences/app/src/main/java/kr/co/study/sharedpreferences/SettingFragment.kt
@@ -0,0 +1,10 @@
+package kr.co.study.sharedpreferences
+
+import android.os.Bundle
+
+
+class SettingFragment: PreferenceFragmentCompat() {
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        addPreferencesFromResource(R.xml.preferences)
+    }
+}
