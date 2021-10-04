9368d146f7918ce0a1d493c04606ac63c2e589b7
snack655 <80940200+snack655@users.noreply.github.com>
2021-10-04 20:58:57 +0900

    STUDY : Resource
    2021-10-04

---
+++ Ch9_Resource/app/src/main/java/org/techtown/ch9_resource/MainActivity.kt
@@ -0,0 +1,11 @@
+package org.techtown.ch9_resource
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
