b43a76b2f615b1cb6926d02018203a66d024fd50
snack655 <80940200+snack655@users.noreply.github.com>
2022-02-17 18:29:44 +0900

    Add : Source file to training
    2022-02-17

--- Ch15_Service/app/src/main/java/kr/co/study/ch15_service/MainActivity.kt
+++ Ch15_Service/app/src/main/java/kr/co/study/ch15_service/MainActivity.kt
@@ -1,11 +1,108 @@
-package kr.co.study.ch15_service
+package com.example.ch15_service
 
+import android.annotation.TargetApi
+import android.app.job.JobInfo
+import android.app.job.JobScheduler
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.content.ServiceConnection
+import android.os.*
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
+import androidx.core.content.getSystemService
+import kotlinx.coroutines.*
+import kr.co.study.ch15_service.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+    var connectionMode = "none"
+
+    //Messenger......
+
+    //aidl...........
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //messenger..........
+        onCreateMessengerService()
+
+        //aidl................
+        onCreateAIDLService()
+
+        //jobscheduler......................
+        onCreateJobScheduler()
+
     }
+
+    override fun onStop() {
+        super.onStop()
+        if(connectionMode === "messenger"){
+            onStopMessengerService()
+        }else if(connectionMode === "aidl"){
+            onStopAIDLService()
+        }
+        connectionMode="none"
+        changeViewEnable()
+    }
+
+    fun changeViewEnable() = when (connectionMode) {
+        "messenger" -> {
+            binding.messengerPlay.isEnabled = false
+            binding.aidlPlay.isEnabled = false
+            binding.messengerStop.isEnabled = true
+            binding.aidlStop.isEnabled = false
+        }
+        "aidl" -> {
+            binding.messengerPlay.isEnabled = false
+            binding.aidlPlay.isEnabled = false
+            binding.messengerStop.isEnabled = false
+            binding.aidlStop.isEnabled = true
+        }
+        else -> {
+            //초기상태. stop 상태. 두 play 버튼 활성상태
+            binding.messengerPlay.isEnabled = true
+            binding.aidlPlay.isEnabled = true
+            binding.messengerStop.isEnabled = false
+            binding.aidlStop.isEnabled = false
+
+            binding.messengerProgress.progress = 0
+            binding.aidlProgress.progress = 0
+        }
+    }
+
+    //messenger handler ......................
+
+    //messenger connection ....................
+
+
+
+    private fun onCreateMessengerService() {
+
+    }
+    private fun onStopMessengerService() {
+
+    }
+
+    //aidl connection .......................
+
+
+    private fun onCreateAIDLService() {
+
+    }
+    private fun onStopAIDLService() {
+
+    }
+
+    //JobScheduler
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    private fun onCreateJobScheduler(){
+
+    }
+
 }
