522e165b2eab7f15b6e2c936f8292e7539fe813a
snack655 <cmjcmj1230@gmail.com>
2022-03-26 22:59:00 +0900

    Add : MainActivity and ResultActivlty
    2022-03-26

---
+++ aop-part2-chapter01/app/src/main/java/fastcampus/aop/part2/chapter01/ResultActivity.kt
@@ -0,0 +1,14 @@
+package fastcampus.aop.part2.chapter01
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ResultActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+
+
+
+    }
+}
