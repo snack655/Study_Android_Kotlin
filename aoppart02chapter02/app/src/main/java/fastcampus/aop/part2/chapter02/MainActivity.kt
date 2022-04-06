a01f4c946141516d9a5a3e155d206f2a9f8bce2c
snack655 <cmjcmj1230@gmail.com>
2022-04-06 19:18:10 +0900

    Add : Resource
    2022-04-06

--- aoppart02chapter02/app/src/main/java/fastcampus/aop/part2/chapter02/MainActivity.kt
+++ aoppart02chapter02/app/src/main/java/fastcampus/aop/part2/chapter02/MainActivity.kt
@@ -7,6 +7,7 @@ import android.widget.Button
 import android.widget.NumberPicker
 import android.widget.TextView
 import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.core.view.isVisible
 
 class MainActivity : AppCompatActivity() {
@@ -62,9 +63,9 @@ class MainActivity : AppCompatActivity() {
 
                 textView.text = number.toString()
                 textView.isVisible = true
-            }
 
-            Log.d( "MainActivity", list.toString())
+                setNumberBackground(number, textView)
+            }
         }
     }
     private fun initAddButton() {
@@ -88,7 +89,18 @@ class MainActivity : AppCompatActivity() {
             textView.isVisible = true
             textView.text = numberPicker.value.toString()
 
+            setNumberBackground(numberPicker.value, textView)
             pickNumberSet.add(numberPicker.value)
+        }
+    }
+
+    private fun setNumberBackground(number: Int, textView: TextView) {
+        when(number) {
+            in 1..10 -> textView.background = ContextCompat.getDrawable(this, R.drawable.circle_yello)
+            in 11..20 -> textView.background = ContextCompat.getDrawable(this, R.drawable.circle_blue)
+            in 21..30 -> textView.background = ContextCompat.getDrawable(this, R.drawable.circle_red)
+            in 31..40 -> textView.background = ContextCompat.getDrawable(this, R.drawable.circle_gray)
+            else -> textView.background = ContextCompat.getDrawable(this, R.drawable.circle_green)
         }
     }
 
