573e7fd9d79da8f85739c6b34a41d74fb5800f6d
snack655 <cmjcmj1230@gmail.com>
2022-05-06 16:37:23 +0900

    Add : Secret Diary main function
    2022-05-06

---
+++ SecretDiary/app/src/main/java/kr/study/android/secretdiary/DiaryActivity.kt
@@ -0,0 +1,12 @@
+package kr.study.android.secretdiary
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class DiaryActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_diary)
+    }
+}
