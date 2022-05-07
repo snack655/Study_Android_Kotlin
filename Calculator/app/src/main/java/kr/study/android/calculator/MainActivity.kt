30bea9b16ad3b0a6d7c7b61301bfea1f2b1e9acc
snack655 <cmjcmj1230@gmail.com>
2022-05-07 16:53:31 +0900

    Add : Calculator layout
    2022-05-07

---
+++ Calculator/app/src/main/java/kr/study/android/calculator/MainActivity.kt
@@ -0,0 +1,28 @@
+package kr.study.android.calculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun buttonClicked(v: View) {
+
+    }
+
+    fun resultButtonClicked(v: View) {
+
+    }
+
+    fun historyButtonClicked(v: View) {
+
+    }
+
+    fun clearButtonClicked(v: View) {
+
+    }
+}
