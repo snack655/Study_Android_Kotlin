4e7f92ef077a65532d89d55232a39aa8d8b54ab3
snack655 <cmjcmj1230@gmail.com>
2022-05-28 16:25:29 +0900

    Add : Message token
    2022-05-28

--- aoppart3chapter01/app/src/main/java/fastcampus/aop/part3/chapter01/MainActivity.kt
+++ aoppart3chapter01/app/src/main/java/fastcampus/aop/part3/chapter01/MainActivity.kt
@@ -3,6 +3,7 @@ package fastcampus.aop.part3.chapter01
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import com.google.firebase.messaging.FirebaseMessaging
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,5 +18,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initFirebase()
+    }
+
+    private fun initFirebase() {
+        FirebaseMessaging.getInstance().token
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    firebaseToken.text = task.result
+                }
+            }
     }
 }
---
+++ aoppart3chapter01/app/src/main/java/fastcampus/aop/part3/chapter01/MyFirebaseMessagingService.kt
@@ -0,0 +1,14 @@
+package fastcampus.aop.part3.chapter01
+
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+class MyFirebaseMessagingService : FirebaseMessagingService() {
+    override fun onNewToken(token: String) {
+        super.onNewToken(token)
+    }
+
+    override fun onMessageReceived(message: RemoteMessage) {
+        super.onMessageReceived(message)
+    }
+}
