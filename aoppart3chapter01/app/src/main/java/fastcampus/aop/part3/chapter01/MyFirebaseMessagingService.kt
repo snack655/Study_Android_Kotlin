5db346efd932f9426ee1c3bca6a01499510da30a
snack655 <cmjcmj1230@gmail.com>
2022-05-30 08:26:51 +0900

    Add : Notification function
    2022-05-30

--- aoppart3chapter01/app/src/main/java/fastcampus/aop/part3/chapter01/MyFirebaseMessagingService.kt
+++ aoppart3chapter01/app/src/main/java/fastcampus/aop/part3/chapter01/MyFirebaseMessagingService.kt
@@ -1,5 +1,13 @@
 package fastcampus.aop.part3.chapter01
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
+import android.widget.RemoteViews
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
@@ -8,7 +16,83 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
         super.onNewToken(token)
     }
 
-    override fun onMessageReceived(message: RemoteMessage) {
-        super.onMessageReceived(message)
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        super.onMessageReceived(remoteMessage)
+
+        createNotificationChannel()
+
+        val type = remoteMessage.data["type"]
+            ?.let { NotificationType.valueOf(it) }
+        val title = remoteMessage.data["title"]
+        val message = remoteMessage.data["message"]
+
+        type ?: return
+
+        NotificationManagerCompat.from(this)
+            .notify(type.id, createNotification(type, title, message))
+    }
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                CHANNEL_ID,
+                CHANNEL_NAME,
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+            channel.description = CHANNEL_DESCRIPTION
+
+            (getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager)
+                .createNotificationChannel(channel)
+        }
+    }
+
+    private fun createNotification(
+        type: NotificationType,
+        title: String?,
+        message: String?
+    ): Notification {
+        val notificationBuilder =  NotificationCompat.Builder(this, CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_baseline_circle_notifications_24)
+            .setContentTitle(title)
+            .setContentText(message)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+
+        when(type) {
+            NotificationType.NORMAL -> Unit
+            NotificationType.EXPANDABLE -> {
+                notificationBuilder.setStyle(
+                    NotificationCompat.BigTextStyle()
+                        .bigText(
+                            "🎉🎊🎁🎈🤠😍🤩🕺💃👫👭👬🧑‍🤝‍🧑✌🤘🤟" +
+                                    "👈👉👆👍🙌👏🍰🎂🧁🥧🍮🍭🍬" +
+                                    "🍺🍻🍷🥂🥃🍸🍹🍾🍽🎃🎭🪩🎤🎧🎼🎹🥁🎷" +
+                                    "👈👉👆👍🙌👏🍰🎂🧁🥧🍮🍭🍬" +
+                                    "👈👉👆👍🙌👏🍰🎂🧁🥧🍮🍭🍬"
+                        )
+                )
+            }
+            NotificationType.CUSTOM -> {
+                notificationBuilder
+                    .setStyle(NotificationCompat.DecoratedCustomViewStyle())
+                    .setCustomContentView(
+                        RemoteViews(
+                            packageName,
+                            R.layout.view_custom_notification
+                        ).apply {
+                            setTextViewText(R.id.title, title)
+                            setTextViewText(R.id.message, message)
+                        }
+                    )
+            }
+            else -> {}
+        }
+
+        return notificationBuilder.build()
+    }
+
+    companion object {
+        private const val CHANNEL_NAME = "Emoji Party"
+        private const val CHANNEL_DESCRIPTION = "Emoji Party를 위한 채널"
+        private const val CHANNEL_ID = "Channel Id"
     }
 }
