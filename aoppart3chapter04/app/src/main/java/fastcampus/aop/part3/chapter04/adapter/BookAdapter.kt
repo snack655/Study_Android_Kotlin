9fddc02bb699e1f337bb916d984db4fd1430ffab
snack655 <cmjcmj1230@gmail.com>
2022-06-13 09:01:05 +0900

    Add : Item view

--- aoppart3chapter04/app/src/main/java/fastcampus/aop/part3/chapter04/adapter/BookAdapter.kt
+++ aoppart3chapter04/app/src/main/java/fastcampus/aop/part3/chapter04/adapter/BookAdapter.kt
@@ -5,6 +5,7 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import fastcampus.aop.part3.chapter04.databinding.ItemBookBinding
 import fastcampus.aop.part3.chapter04.model.Book
 
@@ -13,7 +14,12 @@ class BookAdapter  : ListAdapter<Book, BookAdapter.BookItemViewHolder>(diffUtil)
     inner class BookItemViewHolder(private val binding: ItemBookBinding): RecyclerView.ViewHolder(binding.root) {
         fun bind(bookModel: Book) {
             binding.titleTextView.text = bookModel.title
+            binding.descriptionTextView.text = bookModel.description
 
+            Glide
+                .with(binding.coverImageView.context)
+                .load(bookModel.coverSmallUrl)
+                .into(binding.coverImageView)
         }
     }
 
