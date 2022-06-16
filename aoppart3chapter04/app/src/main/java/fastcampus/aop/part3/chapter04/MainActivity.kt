8d1f50ec1d494b337526d544e3f9fcf39e779bfa
snack655 <cmjcmj1230@gmail.com>
2022-06-16 13:19:55 +0900

    Add : Data base to save search record

--- aoppart3chapter04/app/src/main/java/fastcampus/aop/part3/chapter04/MainActivity.kt
+++ aoppart3chapter04/app/src/main/java/fastcampus/aop/part3/chapter04/MainActivity.kt
@@ -5,11 +5,15 @@ import android.os.Bundle
 import android.util.Log
 import android.view.KeyEvent
 import android.view.MotionEvent
+import androidx.core.view.isVisible
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.room.Room
 import fastcampus.aop.part3.chapter04.adapter.BookAdapter
+import fastcampus.aop.part3.chapter04.adapter.HistoryAdapter
 import fastcampus.aop.part3.chapter04.api.BookService
 import fastcampus.aop.part3.chapter04.databinding.ActivityMainBinding
 import fastcampus.aop.part3.chapter04.model.BestSellerDto
+import fastcampus.aop.part3.chapter04.model.History
 import fastcampus.aop.part3.chapter04.model.SearchBookDto
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
@@ -23,13 +27,23 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var adapter: BookAdapter
+    private lateinit var historyAdapter: HistoryAdapter
     private lateinit var bookService: BookService
+
+    private lateinit var db: AppDatabase
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         initBookRecyclerView()
+        initHistoryRecyclerView()
+
+        db = Room.databaseBuilder(
+            applicationContext,
+            AppDatabase::class.java,
+            "BookSearchDB"
+        ).build()
 
         val interceptor = HttpLoggingInterceptor()
         interceptor.level = HttpLoggingInterceptor.Level.BODY
@@ -70,14 +84,7 @@ class MainActivity : AppCompatActivity() {
 
             })
 
-        binding.searchEditText.setOnKeyListener { v, keyCode, event ->
-            if (keyCode == KeyEvent.KEYCODE_ENTER && event.action == MotionEvent.ACTION_DOWN) {
-                search(binding.searchEditText.text.toString())
-                return@setOnKeyListener true
-            }
 
-            return@setOnKeyListener false
-        }
     }
 
     private fun search(keyword: String) {
@@ -87,6 +94,9 @@ class MainActivity : AppCompatActivity() {
                     call: Call<SearchBookDto>,
                     response: Response<SearchBookDto>
                 ) {
+                    hideHistoryView()
+                    saveSearchKeyword(keyword)
+
                     if (response.isSuccessful.not()) {
                         Log.e(TAG, "NOT!! SUCCESS")
                         return
@@ -96,15 +106,73 @@ class MainActivity : AppCompatActivity() {
                 }
 
                 override fun onFailure(call: Call<SearchBookDto>, t: Throwable) {
+                    hideHistoryView()
                     Log.e(TAG, t.toString(), )
                 }
             })
+    }
+
+    private fun saveSearchKeyword(keyword: String) {
+        Thread {
+            db.historyDao().insertHistory(History(null, keyword))
+        }.start()
     }
 
     private fun initBookRecyclerView() {
         adapter = BookAdapter()
         binding.bookRecyclerView.layoutManager = LinearLayoutManager(this)
         binding.bookRecyclerView.adapter = adapter
+    }
+
+    private fun initHistoryRecyclerView() {
+        historyAdapter = HistoryAdapter {
+            deleteSearchKeyword(it)
+        }
+
+        binding.historyRecyclerView.layoutManager = LinearLayoutManager(this)
+        binding.historyRecyclerView.adapter = historyAdapter
+        
+        initSearchEditText()
+    }
+
+    private fun initSearchEditText() {
+        binding.searchEditText.setOnKeyListener { v, keyCode, event ->
+            if (keyCode == KeyEvent.KEYCODE_ENTER && event.action == MotionEvent.ACTION_DOWN) {
+                search(binding.searchEditText.text.toString())
+                return@setOnKeyListener true
+            }
+
+            return@setOnKeyListener false
+        }
+        
+        binding.searchEditText.setOnTouchListener { view, event ->
+            if (event.action == MotionEvent.ACTION_DOWN)
+                showHistoryView()
+            return@setOnTouchListener false
+        }
+    }
+
+    private fun showHistoryView() {
+        Thread {
+            val keywords = db.historyDao().getAll().reversed()
+
+            runOnUiThread {
+                binding.historyRecyclerView.isVisible = true
+                historyAdapter.submitList(keywords.orEmpty())
+            }
+        }.start()
+        binding.historyRecyclerView.isVisible = true
+    }
+
+    private fun hideHistoryView() {
+        binding.historyRecyclerView.isVisible = false
+    }
+
+    private fun deleteSearchKeyword(keyword: String) {
+        Thread {
+            db.historyDao().delete(keyword)
+            showHistoryView()
+        }.start()
     }
 
     companion object {
---
+++ aoppart3chapter04/app/src/main/java/fastcampus/aop/part3/chapter04/adapter/HistoryAdapter.kt
@@ -0,0 +1,45 @@
+package fastcampus.aop.part3.chapter04.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import fastcampus.aop.part3.chapter04.databinding.ItemBookBinding
+import fastcampus.aop.part3.chapter04.databinding.ItemHistoryBinding
+import fastcampus.aop.part3.chapter04.model.Book
+import fastcampus.aop.part3.chapter04.model.History
+
+class HistoryAdapter(val historyDeleteClickedListener: (String) -> Unit)  : ListAdapter<History, HistoryAdapter.HistoryItemViewHolder>(diffUtil) {
+
+    inner class HistoryItemViewHolder(private val binding: ItemHistoryBinding): RecyclerView.ViewHolder(binding.root) {
+        fun bind(historyModel: History) {
+            binding.historyKeywordTextView.text = historyModel.keyword
+
+            binding.historyKeywordDeleteButton.setOnClickListener {
+                historyDeleteClickedListener(historyModel.keyword.orEmpty())
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HistoryItemViewHolder {
+        return HistoryItemViewHolder(ItemHistoryBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+    }
+
+    override fun onBindViewHolder(holder: HistoryItemViewHolder, position: Int) {
+        holder.bind(currentList[position])
+    }
+
+    companion object {
+        val diffUtil = object : DiffUtil.ItemCallback<History>() {
+            override fun areItemsTheSame(oldItem: History, newItem: History): Boolean {
+                return oldItem == newItem
+            }
+
+            override fun areContentsTheSame(oldItem: History, newItem: History): Boolean {
+                return oldItem.keyword == newItem.keyword
+            }
+        }
+    }
+}
