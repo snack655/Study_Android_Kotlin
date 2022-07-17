25954ae03016f0460a11c2654503b3f820994c3b
snack655 <cmjcmj1230@gmail.com>
2022-07-17 15:28:15 +0900

    Add : Receive goods list to firebase database

--- aop-part3-chapter06/app/src/main/java/fastcampus/aop/part3/chapter06/home/HomeFragment.kt
+++ aop-part3-chapter06/app/src/main/java/fastcampus/aop/part3/chapter06/home/HomeFragment.kt
@@ -1,16 +1,47 @@
 package fastcampus.aop.part3.chapter06.home
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.ChildEventListener
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+import fastcampus.aop.part3.chapter06.DBKey.Companion.DB_ARTICLES
 import fastcampus.aop.part3.chapter06.R
 import fastcampus.aop.part3.chapter06.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment(R.layout.fragment_home) {
 
-    private var binding: FragmentHomeBinding? = null
+    private lateinit var articleDB: DatabaseReference
     private lateinit var articleAdapter: ArticleAdapter
+
+    private val articleList = mutableListOf<ArticleModel>()
+    private val listener = object : ChildEventListener {
+        override fun onChildAdded(snapshot: DataSnapshot, previousChildName: String?) {
+            val articleModel = snapshot.getValue(ArticleModel::class.java)
+            articleModel ?: return
+
+            articleList.add(articleModel)
+            articleAdapter.submitList(articleList)
+        }
+
+        override fun onChildChanged(snapshot: DataSnapshot, previousChildName: String?) {}
+        override fun onChildRemoved(snapshot: DataSnapshot) {}
+        override fun onChildMoved(snapshot: DataSnapshot, previousChildName: String?) {}
+        override fun onCancelled(error: DatabaseError) {}
+    }
+
+    private var binding: FragmentHomeBinding? = null
+    private val auth: FirebaseAuth by lazy {
+        Firebase.auth
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -18,14 +49,26 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
         val fragmentHomeBinding = FragmentHomeBinding.bind(view)
         binding = fragmentHomeBinding
 
+        articleList.clear()
+        articleDB = Firebase.database.reference.child(DB_ARTICLES)
         articleAdapter = ArticleAdapter()
-        articleAdapter.submitList(mutableListOf<ArticleModel>().apply {
-            add(ArticleModel("0", "aaaa", 1000000, "50000원", ""))
-            add(ArticleModel("0", "bbbb", 2000000, "100000원", ""))
-        })
 
         fragmentHomeBinding.articleRecyclerView.layoutManager = LinearLayoutManager(context)
         fragmentHomeBinding.articleRecyclerView.adapter = articleAdapter
 
+        articleDB.addChildEventListener(listener)
+
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    override fun onResume() {
+        super.onResume()
+        articleAdapter.notifyDataSetChanged()
+    }
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        articleDB.removeEventListener(listener)
     }
 }
