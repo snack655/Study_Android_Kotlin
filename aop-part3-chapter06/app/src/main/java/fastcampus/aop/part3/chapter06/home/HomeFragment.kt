78b3ff4f24ac8a236018f0b103bc3d22cad905fd
snack655 <cmjcmj1230@gmail.com>
2022-07-22 19:44:47 +0900

    Add : auth

--- aop-part3-chapter06/app/src/main/java/fastcampus/aop/part3/chapter06/home/HomeFragment.kt
+++ aop-part3-chapter06/app/src/main/java/fastcampus/aop/part3/chapter06/home/HomeFragment.kt
@@ -60,13 +60,12 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
 
         fragmentHomeBinding.addFloatingButton.setOnClickListener {
             context?.let {
-                // todo 로그인 구현 후에 주석 지우기
-                //if (auth.currentUser != null) {
+                if (auth.currentUser != null) {
                     val intent = Intent(it, AddArticleActivity::class.java)
                     startActivity(intent)
-                //} else {
-                //    Snackbar.make(view, "로그인 후 사용해주세요", Snackbar.LENGTH_LONG).show()
-                //}
+                } else {
+                    Snackbar.make(view, "로그인 후 사용해주세요", Snackbar.LENGTH_LONG).show()
+                }
             }
         }
 
