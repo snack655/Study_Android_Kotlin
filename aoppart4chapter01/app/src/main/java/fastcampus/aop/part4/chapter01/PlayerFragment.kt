db3378049f1d01e008c0c02b60b32d6bb08aa754
snack655 <cmjcmj1230@gmail.com>
2022-08-06 22:30:23 +0900

    Add : 비디오 누를 시 Motion Layout 활성화

--- aoppart4chapter01/app/src/main/java/fastcampus/aop/part4/chapter01/PlayerFragment.kt
+++ aoppart4chapter01/app/src/main/java/fastcampus/aop/part4/chapter01/PlayerFragment.kt
@@ -1,15 +1,26 @@
 package fastcampus.aop.part4.chapter01
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.constraintlayout.motion.widget.MotionLayout
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import fastcampus.aop.part4.chapter01.adapter.VideoAdapter
 import fastcampus.aop.part4.chapter01.databinding.FragmentPlayerBinding
+import fastcampus.aop.part4.chapter01.dto.VideoDto
+import fastcampus.aop.part4.chapter01.service.VideoService
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import kotlin.math.abs
 
 class PlayerFragment : Fragment(R.layout.fragment_player) {
 
     private var binding: FragmentPlayerBinding? = null
+    private lateinit var videoAdapter: VideoAdapter
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -17,6 +28,13 @@ class PlayerFragment : Fragment(R.layout.fragment_player) {
         val fragmentPlayerBinding = FragmentPlayerBinding.bind(view)
         binding = fragmentPlayerBinding
 
+        initMotionLayoutEvent(fragmentPlayerBinding)
+        initRecyclerView(fragmentPlayerBinding)
+
+        getVideoList()
+    }
+
+    private fun initMotionLayoutEvent(fragmentPlayerBinding: FragmentPlayerBinding) {
         fragmentPlayerBinding.playerMotionLayout.setTransitionListener(object: MotionLayout.TransitionListener {
             override fun onTransitionStarted(motionLayout: MotionLayout?, startId: Int, endId: Int) {}
 
@@ -27,16 +45,54 @@ class PlayerFragment : Fragment(R.layout.fragment_player) {
                     }
                 }
             }
-
-            override fun onTransitionCompleted(motionLayout: MotionLayout?, currentId: Int) {
-
-            }
-
-            override fun onTransitionTrigger(motionLayout: MotionLayout?, triggerId: Int, positive: Boolean, progress: Float) {
-
-            }
-
+            override fun onTransitionCompleted(motionLayout: MotionLayout?, currentId: Int) {}
+            override fun onTransitionTrigger(motionLayout: MotionLayout?, triggerId: Int, positive: Boolean, progress: Float) {}
         })
+    }
+
+    private fun initRecyclerView(fragmentPlayerBinding: FragmentPlayerBinding) {
+
+        videoAdapter = VideoAdapter { url, title ->
+            play(url, title)
+        }
+
+        fragmentPlayerBinding.fragmentRecyclerView.apply {
+            adapter = videoAdapter
+            layoutManager = LinearLayoutManager(context)
+        }
+    }
+
+    private fun getVideoList() {
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://run.mocky.io/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        retrofit.create(VideoService::class.java).also {
+            it.listVideos()
+                .enqueue(object : Callback<VideoDto> {
+                    override fun onResponse(call: Call<VideoDto>, response: Response<VideoDto>) {
+                        if (response.isSuccessful.not()) {
+                            Log.d("MainActivity", "response fail")
+                            return
+                        }
+                        response.body()?.let { videoDto ->
+                            Log.d("MainActivity", videoDto.toString())
+                            videoAdapter.submitList(videoDto.videos)
+                        }
+                    }
+                    override fun onFailure(call: Call<VideoDto>, t: Throwable) {
+                        // 예외처리
+                    }
+                })
+        }
+    }
+
+    fun play(url: String, title: String) {
+        binding?.let {
+            it.playerMotionLayout.transitionToEnd()
+            it.bottomTitleTextView.text = title
+        }
     }
 
     override fun onDestroy() {
