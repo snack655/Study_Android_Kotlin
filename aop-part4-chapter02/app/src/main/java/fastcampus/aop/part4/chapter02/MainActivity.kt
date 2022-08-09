5032f9a1d2966cf13d2999fca849bed8f496d01c
snack655 <cmjcmj1230@gmail.com>
2022-08-09 15:43:13 +0900

    Add : 음악 스트리밍 앱 기본 UI 세팅

---
+++ aop-part4-chapter02/app/src/main/java/fastcampus/aop/part4/chapter02/MainActivity.kt
@@ -0,0 +1,15 @@
+package fastcampus.aop.part4.chapter02
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, PlayerFragment.newInstance())
+            .commit()
+    }
+}
