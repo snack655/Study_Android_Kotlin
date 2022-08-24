027acb4051a53539ba3337a77e4c8df3f173a6a9
최민재 <cmjcmj1230@gmail.com>
2022-08-24 09:33:23 +0900

    Add : 내 위치 불러오기

--- Aoppart4chapter03/app/src/main/java/aop/fastcampus/part4/chapter03/MapActivity.kt
+++ Aoppart4chapter03/app/src/main/java/aop/fastcampus/part4/chapter03/MapActivity.kt
@@ -1,16 +1,28 @@
 package aop.fastcampus.part4.chapter03
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import aop.fastcampus.part4.chapter03.databinding.ActivityMainBinding
+import aop.fastcampus.part4.chapter03.model.LocationLatLngEntity
 import aop.fastcampus.part4.chapter03.model.SearchResultEntity
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.MapView
 import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
+import kotlinx.android.synthetic.main.activity_map.*
 
 class MapActivity: AppCompatActivity(), OnMapReadyCallback {
 
@@ -20,9 +32,14 @@ class MapActivity: AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var searchResult: SearchResultEntity
 
+    private lateinit var locationManager: LocationManager
+
+    private lateinit var myLocationListener: MyLocationListener
+
     companion object {
-        val SEARCH_RESULT_EXTRA_KEY = "SEARCH_RESULT_EXTRA_KEY"
-        val CAMERA_ZOOM_LEVEL = 17f
+        const val SEARCH_RESULT_EXTRA_KEY = "SEARCH_RESULT_EXTRA_KEY"
+        const val CAMERA_ZOOM_LEVEL = 17f
+        const val PERMISSION_REQUEST_CODE = 101
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -36,10 +53,18 @@ class MapActivity: AppCompatActivity(), OnMapReadyCallback {
                 setupGoogleMap()
             }
         }
+
+        bindViews()
+    }
+
+    private fun bindViews() = with(binding) {
+        currentLocationButton.setOnClickListener {
+            getMyLocation()
+        }
     }
 
     private fun setupGoogleMap() {
-        val mapFragment = findViewById<MapView>(R.id.mapFragment)
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.mapFragment) as SupportMapFragment
         mapFragment.getMapAsync(this)
     }
 
@@ -63,5 +88,72 @@ class MapActivity: AppCompatActivity(), OnMapReadyCallback {
         map.moveCamera(CameraUpdateFactory.newLatLngZoom(positionLatLng, CAMERA_ZOOM_LEVEL))
 
         return map.addMarker(markerOptions)
+    }
+
+    private fun getMyLocation() {
+        if (::locationManager.isInitialized.not()) {
+            locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        }
+
+        val isGpsEnable = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
+        if (isGpsEnable) {
+            if (ContextCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(
+                        Manifest.permission.ACCESS_FINE_LOCATION,
+                        Manifest.permission.ACCESS_COARSE_LOCATION
+                    ),
+                    PERMISSION_REQUEST_CODE
+                )
+            } else {
+                setMyLocationListener()
+            }
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun setMyLocationListener() {
+        val minTime = 1500L
+        val minDistance = 100f
+
+        if (!::myLocationListener.isInitialized) {
+            myLocationListener = MyLocationListener()
+        }
+
+        with(locationManager) {
+            requestLocationUpdates(
+                LocationManager.GPS_PROVIDER,
+                minTime, minDistance, myLocationListener
+            )
+            requestLocationUpdates(
+                LocationManager.GPS_PROVIDER,
+                minTime, minDistance, myLocationListener
+            )
+        }
+    }
+
+    private fun onCurrentLocationChanged(locationLatLngEntity: LocationLatLngEntity) {
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(
+            LatLng(
+            locationLatLngEntity.latitude.toDouble(),
+                locationLatLngEntity.longitude.toDouble(),
+        ), CAMERA_ZOOM_LEVEL))
+    }
+
+    inner class MyLocationListener: LocationListener {
+        override fun onLocationChanged(location: Location) {
+            val locationLatLngEntity = LocationLatLngEntity(
+                location.latitude.toFloat(),
+                location.longitude.toFloat()
+            )
+            onCurrentLocationChanged(locationLatLngEntity)
+        }
     }
 }
